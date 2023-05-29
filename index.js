8f15202c3b8f1d203b3ae1546aca57a1b663342f
harjas816 <harjas816@gmail.com>
2023-05-29 14:07:38 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers,oneWeWant){
+    return drivers.filter(x => x.toUpperCase() === oneWeWant.toUpperCase())
+}
+
+function fuzzyMatch(drivers, letterFilter){
+    return drivers.filter(x => letterFilter.includes(x[0]))
+}
+
+function matchName(drivers, criteria){
+    return drivers.filter(x => x.name === criteria)
+}
